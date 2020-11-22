0ad7a7de2d707cc7b05e579176c0bbac17396911
RAZ0229 <raja.zafar229@gmail.com>
2020-11-22 14:30:11 +0500

    Update braille-translation.py

--- braille-translation.py
+++ braille-translation.py
@@ -16,5 +16,4 @@ def solution(s):
         to_convert += x
     to_convert = to_convert.replace(" ", "000000").replace("a", "100000").replace("b","110000").replace("c","100100").replace("d","100110").replace("e","100010").replace("f","110100").replace("g","110110").replace("h","110010").replace("i","010100").replace("j","010110").replace("k","101000").replace("l","111000").replace("m","101100").replace("n","101110").replace("o","101010").replace("p","111100").replace("q","111110").replace("r","111010").replace("s","011100").replace("t","011110").replace("u","101001").replace("v","111001").replace("w","010111").replace("x","101101").replace("y","101111").replace("z","101011")
     return to_convert
-    
-print(solution("B")) #Type input here
+  #All Test Cases Passed
